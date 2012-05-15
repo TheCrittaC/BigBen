41d2191086b1727dda042bb8715aeb90056cb01f
Christopher <the.critta.c@gmail.com>
2012-05-14 21:49:50 -0400

    did some logic work

--- BigBen.c
+++ BigBen.c
@@ -1,4 +1,36 @@
-#include <stdio.h>
-#include <time.h>
-void main(){
+#include <stdio.h> //for printf and scanf
+#include <time.h> //for the time things
+#include <unistd.h> //for sleep
+#include <string.h> //for the string comparison functions
+int getHour(){
+  time_t rawtime;
+  struct tm *timeinfo;
+  time(&rawtime);
+  timeinfo= gmtime(&rawtime); /*converts the current time into GMT time 
+					so Big Ben chimes just as he would in
+					London.*/
+  return timeinfo->tm_hour % 12; //This way, we get a maximum of 12 BONGs
+}
+
+int main(){
+  int i;
+  int currentHour = getHour(); //sets the current hour so we can monitor it for changes
+  char input[1];
+  int equal;
+  while (0 == 0){
+    scanf("%s",&input);
+    equal = strncmp(input, ".time",100);
+    if (equal == 0)
+      printf("OI IT'S %d BONG\n", getHour());
+    if (currentHour != getHour()){
+      currentHour = getHour(); //changes the hour so we don't repeat the BONG
+      for (i = 0; i < getHour(); i++)
+	//printf("BONG");
+      printf("\n");
+    }
+  }
   
+  return 0;
+  
+}
+
