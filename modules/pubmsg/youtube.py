58c579cbdf5f105456e4e1274106911d7a90730a
Ryan <ryan@swiftler.com>
2013-06-23 11:15:49 +0100

    Fixed HTML encoding

--- modules/pubmsg/youtube.py
+++ modules/pubmsg/youtube.py
@@ -1,6 +1,7 @@
 import urllib2
 from xml.dom import minidom
 import re
+import HTMLParser
 class youtube:
     def on_pubmsg(self, nick, connection, event):   
         message = event.arguments()[0]
@@ -12,7 +13,10 @@ class youtube:
         if message.startswith(".yt"):
             arg1 = arg1.replace(" ", "+")
             message = self.getVideo(arg1)
-            connection.privmsg(event.target(), message)
+            try:
+                connection.privmsg(event.target(), message)
+            except:
+                connection.privmsg(event.target(), "Nothing found")
 
     def getVideo(self, keyword):
         url = 'https://gdata.youtube.com/feeds/api/videos?q={0}&max-results=1&v=2'.format(keyword)
@@ -23,8 +27,10 @@ class youtube:
             title = title[1].toxml()
         except IndexError:
             return "Nothing found"
+        html_parser = HTMLParser.HTMLParser()
         title = re.sub('<[^>]*>', '', title)
+        title = html_parser.unescape(title)
         url = url[0].attributes['url'].value
         url = re.sub('&feature.+', '', url)
-        message = u"\u00031,00You\u000300,5Tube\u000300,1 {0} - {1}".format(title, url)
+        message = u"\u00031,00You\u000300,5Tube\u0003 {0} - {1}".format(title, url)
         return message
