755c2e139553b752262ae2f98e8e6fb1645aaa1c
kattming <king133@purdue.edu>
2013-07-03 13:08:51 -0500

    added no results error to google search module

--- modules/pubmsg/google.py
+++ modules/pubmsg/google.py
@@ -4,24 +4,33 @@ import json
 import HTMLParser
 import thread
 import os
+import ast
 class google:
     def Gsearch(self, query, connection, event):
+            parser = HTMLParser.HTMLParser()
             url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=' + query
             search_response = urllib.urlopen(url)
             search_results = search_response.read()
             results = json.loads(search_results)
             data = results['responseData']
             hits = data['results']
-            url_1 = hits[0]['url']
-            title_1 = hits[0]["titleNoFormatting"]
-            url_2 = hits[1]['url']
-            title_2 = hits[1]['titleNoFormatting']
-            connection.privmsg(event.target(), (u"{0} :: {1}").format(title_1, url_1))
-            connection.privmsg(event.target(), (u"{0} :: {1}").format(title_2, url_2))
+            try:
+                url_1 = parser.unescape(hits[0]['url'])
+                title_1 = parser.unescape(hits[0]["titleNoFormatting"])
+                connection.privmsg(event.target(), (u"{0} :: {1}").format(title_1, url_1).encode('utf-8'))
+            except:
+                connection.privmsg(event.target(), ("Error: No results found"))
+            try:
+                url_2 = parser.unescape(hits[1]['url'])
+                title_2 = parser.unescape(hits[1]['titleNoFormatting'])
+                connection.privmsg(event.target(), (u"{0} :: {1}").format(title_2, url_2).encode('utf-8'))
+            except:
+                return
+            
     def on_pubmsg(self, nick, connection, event):
         message = event.arguments()[0]
         source = event.source().split('!')[0]
         query = ""
         if message.startswith(".g"):
             query = message[3:]
-            thread.start_new_thread(self.Gsearch(query, connection, event))
+            self.Gsearch(query, connection, event)
